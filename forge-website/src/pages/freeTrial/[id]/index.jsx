59e5d081fc921e9a6e51ec918e69c3d95119a5aa
DelaraGi <Delara.galehdari@linaro.org>
2023-02-21 16:34:24 +0000

    routing test

--- forge-website/src/pages/freeTrial/[id]/index.jsx
+++ forge-website/src/pages/freeTrial/[id]/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-import Navbar from '../../../components/Navbar/index';
 import Typography from '@mui/material/Typography';
 import TrialHeroCard from '../../../components/HeroCard/TrialHeroCard/index';
 
 const Thanks = () => {
   return (
    <>
-   <Navbar/>
     <TrialHeroCard/>
     <Card sx={{ display: 'flex',width:'100%' ,height:'350px',borderRadius:0,mx:'auto'}}>
     <Typography fontSize={14} variant="body1" pr={1} sx={{pl:{xs:1,md:4},display:'flex',mx:'auto',mt:4}}>
