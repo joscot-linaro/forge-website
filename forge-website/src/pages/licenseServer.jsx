346f08725e18c0a7737cbde1a7ea0eb82636e58a
DelaraGi <Delara.galehdari@linaro.org>
2023-02-28 09:14:12 +0000

    fix: text on license server

--- forge-website/src/pages/licenseServer.jsx
+++ forge-website/src/pages/licenseServer.jsx
@@ -25,16 +25,14 @@ const LicenseServer = () => {
 			</Head>
       <CssBaseline />
       <Grid>
-    {/* <Grid flexGrow={2} sx={{backgroundColor:'white',
-    boxSizing:'border-box',m:0,p:0,width:{xs:'min-content',md:'100%',sm:'100%' }
-    , }}> */}
+
     <DownloadHeroCard/>
     <Grid sx={{ mt:2,minHeight:{md:'70.5vh',xs:'100vh'}}}>
     <Grid  sx={{display:'flex',flexDirection:'column',borderRadius:0,borderColor:'white',ml:6,width:'100%',mt:4,mx:'auto',}}>
         <Typography variant="h5"  sx={{mt:2,mb:3,fontWeight:'600',pl:3}}>Linaro License Server</Typography>
         <Typography variant="h6"  sx={{mt:2,mb:3,fontWeight:'600',pl:3}}>Download Linaro Licence Server 22.1.3</Typography>
         <Typography variant="body1"  sx={{mt:2,mb:3,fontWeight:'400',pl:3}}>Use the table below to download the latest version of Linaro Licence Server for your server and HPC toolchain.</Typography>
-        <Typography variant="body1"  sx={{mt:2,mb:3,fontWeight:'400',pl:3}}>For more information, see <Typography sx={{textDecoration:'none',color:'#2596be'}} component='a' href='https://docs.linaroforge.com/22.1.3/html/101169_arm-licence-server-user-guide/use_arm_licence_server/index.html'>Use Arm Licence Server</Typography>.</Typography>
+        <Typography variant="body1"  sx={{mt:2,mb:3,fontWeight:'400',pl:3}}>For more information, see <Typography sx={{textDecoration:'none',color:'#2596be'}} component='a' href='https://docs.linaroforge.com/22.1.3/html/101169_arm-licence-server-user-guide/use_arm_licence_server/index.html'>Use Linaro Licence Server</Typography>.</Typography>
         </Grid>
         <LicenseServerTable />
         <Button onClick={()=>router.push('/LicenceServerReleasehistory')} sx={{color:'white',background:'#23b1da',m:2,borderRadius:3,'&:hover':{color:'#23b1da'}}}>Release history</Button>
