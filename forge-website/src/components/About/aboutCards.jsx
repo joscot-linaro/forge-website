998b589b32b183ff9115b33de3630ca12a762ed6
Delara Galehdari <delara.galehdari@linaro.org>
2023-05-17 09:18:35 +0100

    fix:abour page view

--- forge-website/src/components/About/aboutCards.jsx
+++ forge-website/src/components/About/aboutCards.jsx
@@ -23,7 +23,7 @@ const AboutCards = () => {
                 <CardContainer data={aboutForge[0]} cardHeight={500} />
             </Grid>
             <Grid item xs={12} sm={8} >
-                <Grid container sx={{ display: 'flex', flexDirection: { sm: 'column', md: 'column', lg: 'row' }, justifyContent: 'center', width: { xl: '80%' } }}>
+                <Grid container sx={{ display: 'flex', flexDirection: { sm: 'column', md: 'column', lg: 'row' }, justifyContent: 'center', width: { xl: '100%' } }}>
                     <Grid item xs={12} sm={6} sx={{ width: '100%', p: { lg: 2, md: 0 }, mt: { xs: 2, md: 0 }, alignSelf: { md: 'center', xs: 'center ' }, alignItems: { md: 'center', xs: 'center ' } }}>
                         <Typography variant='h3' sx={{ textAlign: 'center', fontWeight: 600, color: '#9bcc4c' }}>2016</Typography>
                         <CardContainer data={aboutForge[1]} cardHeight={275} />
@@ -33,7 +33,7 @@ const AboutCards = () => {
                         <CardContainer data={aboutForge[2]} cardHeight={275} />
                     </Grid>
                     <Grid item sx={{
-                        width: { xl: '80%', lg: '87%', xs: '100%' },
+                        width: { xl: '68%', lg: '87%', xs: '100%' },
                         border: '5px solid #e2eec1',
                         mt: { lg: 0, md: 2, xs: 2 },
                         p: { lg: 2, xs: 0 },
